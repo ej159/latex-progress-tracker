d49cb5fbef5e9307b800c05067192ec7e5b9181c
ej159 <edwardgeorgejones@gmail.com>
2020-10-20 14:25:51 +0100

    Fix for multiple tex files

--- updateProgress.sh
+++ updateProgress.sh
@@ -5,21 +5,32 @@
 # export PATH=/usr/local/bin:/Library/TeX/texbin/:$PATH
 
 echo "Updating progress"
-TEX_DOC='responses.tex'
-DOCUMENT='responses.pdf'
+TEX_PATH='/home/edwardjones/git/Thesis'
+TEX_DOC=${TEX_PATH}'/main.tex'
+DOCUMENT='/home/edwardjones/git/Thesis/main.pdf'
 PROGRESSFILE='progress.csv'
+
 
 # Setup CSV if it doesn't exist
 if [ ! -f ${PROGRESSFILE} ]; then
     echo "timestamp,wordcount,pagecount" >> ${PROGRESSFILE}
 fi
 
+WORDCOUNT=0
 
-WORDCOUNT=`texcount -sum -total -merge {$TEX_DOC} | grep "Sum count:" | tr -d "Sum count: "`
+for f in ${TEX_PATH}/*.tex;
+do echo "Processing $f file..";
+	DOCCOUNT=`texcount -sum -total -merge $f | grep "Sum count:" | tr -d "Sum count:"`
+	echo $f:$DOCCOUNT
+	WORDCOUNT=$((WORDCOUNT+DOCCOUNT))
+done
+
+echo $WORDCOUNT
+
 # Use this line in OSX
-PAGECOUNT=`mdls -name kMDItemNumberOfPages -raw ${DOCUMENT}`
+#PAGECOUNT=`mdls -name kMDItemNumberOfPages -raw ${DOCUMENT}`
 # Use this line in Linux
-# PAGECOUNT=`pdfinfo mythesis.pdf | grep Pages | sed 's/[^0-9]*//'`
+PAGECOUNT=`pdfinfo ${DOCUMENT} | grep Pages | sed 's/[^0-9]*//'`
 
 echo `date '+%Y-%m-%d %H:%M:%S'`,$WORDCOUNT,$PAGECOUNT >> $PROGRESSFILE
 echo "Done! Page count ${PAGECOUNT}, word count ${WORDCOUNT}. Written to ${PROGRESSFILE}"
